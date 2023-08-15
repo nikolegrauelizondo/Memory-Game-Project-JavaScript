5065d264700379cc1462c169739e5d9e37a918dd
nikolegrauelizondo <136844598+nikolegrauelizondo@users.noreply.github.com>
2023-08-15 15:18:55 -0600

    Save changes.

--- game/js/controllers/credits/creditsView.js
+++ game/js/controllers/credits/creditsView.js
@@ -7,6 +7,16 @@ export class CreditsView extends ControllerView {
         this.container.id = 'creditsView';
         this.elementsContainer.className = 'creditsView-elementsContainer';
 
-        this.creditsText = p({ innerHTML: 'Add credits here.', className: 'creditsView-text' }, this.elementsContainer);
+        // this.creditsText = p({ innerHTML: 'Add credits here.', className: 'creditsView-text' }, this.elementsContainer);
+
+        this.creditsFirstTitle = p({ innerHTML: 'Team Lead:', className: 'creditsView-first-title' }, this.elementsContainer);
+
+        this.creditsFirstText = p({ innerHTML: 'Esteban Padilla Sánchez', className: 'creditsView-first-text' }, this.elementsContainer);
+
+        this.creditsSecondTitle = p({ innerHTML: 'Programmer:', className: 'creditsView-second-title' }, this.elementsContainer);
+
+        this.creditsSecondText = p({ innerHTML: 'Nikole Grau Elizondo', className: 'creditsView-second-text' }, this.elementsContainer);
+
+        this.creditsThirdTitle = p({ innerHTML: 'Universidad Cenfotec <br> 2023', className: 'creditsView-third-title' }, this.elementsContainer);
     }
 }
